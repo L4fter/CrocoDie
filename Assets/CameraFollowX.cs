d62a5903f5221cf6243a42ba15f51fa198de26ff
Artem <a.korotenko@globalplaygames.com>
2015-04-19 01:57:30 +0300

    camera offset

--- Assets/CameraFollowX.cs
+++ Assets/CameraFollowX.cs
@@ -6,13 +6,16 @@ public class CameraFollowX : MonoBehaviour
 
 	public GameObject Target;
 
+    private float offset;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    offset = transform.position.x - Target.transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		this.transform.position = new Vector3(Target.transform.position.x, this.transform.position.y, this.transform.position.z);
+		this.transform.position = new Vector3(Target.transform.position.x + offset, this.transform.position.y, this.transform.position.z);
 	}
 }
