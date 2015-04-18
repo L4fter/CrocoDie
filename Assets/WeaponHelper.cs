738c5463848f76a1f537cd1b20e296d68ccfc937
g.isachenko <g.isachenko@globalplaygames.com>
2015-04-18 14:26:10 +0300

    gizmo fixed

--- Assets/WeaponHelper.cs
+++ Assets/WeaponHelper.cs
@@ -21,8 +21,8 @@ public class WeaponHelper : MonoBehaviour
 			var color = Gizmos.color;
 			Gizmos.color = new Color(1, 0, 0, 1);
 			Gizmos.DrawCube(
-				this.transform.position + new Vector3(-0.1255093f, -0.02214766f),
-				new Vector3(0.2469447f, 0.4536676f, 1));
+				this.transform.position + (Vector3)component.offset,
+				component.bounds.size);
 			Gizmos.color = color;
 		}
 	}
