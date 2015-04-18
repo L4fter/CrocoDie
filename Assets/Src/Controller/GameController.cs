c7500e63eb0ac5a1fbf7aef14de0ee6c593e7d9e
Artem <a.korotenko@globalplaygames.com>
2015-04-18 14:57:39 +0300

    base connection

--- Assets/Src/Controller/GameController.cs
+++ Assets/Src/Controller/GameController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
 
+    public NetworkView NetworkView;
+
     private bool controlMan;
 
 	// Use this for initialization
@@ -16,9 +18,40 @@ public class GameController : MonoBehaviour
 	
 	}
 
-    public void LoadGame()
+    public void LoadSingleGame()
     {
         controlMan = false;
         Application.LoadLevel("TestLevel");
+    }
+
+    public void LoadServerGame()
+    {
+        Debug.Log("Loading server game...");
+        controlMan = false;
+        NetworkView.RPC("LoadClientGame", RPCMode.All, !controlMan);
+        Application.LoadLevel("TestLevel");
+    }
+
+    public void LoadClientGame(bool controlMan)
+    {
+        Debug.Log("Loading client game...");
+        this.controlMan = controlMan;
+        Application.LoadLevel("TestLevel");
+    }
+
+    public void Host()
+    {
+        var useNat = !Network.HavePublicAddress();
+        var er = Network.InitializeServer(32, 25000, useNat);
+        Debug.Log("Hosting error: " + er);
+    }
+
+    public void Connect()
+    {
+        var er = Network.Connect("127.0.0.1", 25000);
+        if (er == NetworkConnectionError.NoError)
+        {
+            NetworkView.RPC("LoadServerGame", RPCMode.All);
+        }
     }
 }
