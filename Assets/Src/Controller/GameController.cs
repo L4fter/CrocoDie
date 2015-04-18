55f9bda61961b0557038e350a71abc72f0358d52
Artem <a.korotenko@globalplaygames.com>
2015-04-18 14:11:26 +0300

    Basic menu, level loading

---
+++ Assets/Src/Controller/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    private bool controlMan;
+
+	// Use this for initialization
+	void Start () {
+	    DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadGame()
+    {
+        controlMan = false;
+        Application.LoadLevel("TestLevel");
+    }
+}
---
+++ Assets/Src/UI/UIController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Button CreateBtn;
+
+    public Button JoinBtn;
+
+	// Use this for initialization
+	void Start () {
+	    this.CreateBtn.GetComponentInChildren<Text>().text = "Create";
+	    this.JoinBtn.GetComponentInChildren<Text>().text = "Join";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
