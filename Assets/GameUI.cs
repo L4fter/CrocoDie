3a4afc042d2313fb30e072cd4b03c530b47772fb
g.isachenko <g.isachenko@globalplaygames.com>
2015-04-19 13:27:56 +0300

    New images, win window

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -9,7 +9,11 @@ public class GameUI : MonoBehaviour
 
     public Button BackButton;
 
+    public Button NextButton;
+
     public Image Img;
+
+    public Image ImgWin;
 
 	private static GameUI instance;
 
@@ -49,6 +53,19 @@ public class GameUI : MonoBehaviour
             instance.ShowButton(200, instance.RepeatButton, delay);
         }
         instance.ShowButton(-200, instance.BackButton, delay);
+    }
+    public static void Win(int delay)
+    {
+        var obj = instance.ImgWin.gameObject;
+        var pos = obj.transform.position;
+		instance.ImgWin.gameObject.transform.position = new Vector3(pos.x, pos.y + 300, pos.z);
+		instance.ImgWin.gameObject.SetActive(true);
+        LeanTween.move(obj, pos, 1.5f).setEase(LeanTweenType.easeOutElastic).setDelay(delay);
+
+        
+        instance.ShowButton(200, instance.RepeatButton, delay + 1);
+        instance.ShowButton(-200, instance.BackButton, delay);
+        instance.ShowButton(1000, instance.NextButton, delay);
     }
 
     private void ShowButton(int offsetX, Button btn, int delay)
