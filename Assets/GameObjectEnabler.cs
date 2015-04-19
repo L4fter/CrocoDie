a213220fa394873852357da9920a7dc79e9ee74b
g.isachenko <g.isachenko@globalplaygames.com>
2015-04-19 11:36:25 +0300

    Birds feathers

---
+++ Assets/GameObjectEnabler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectEnabler : MonoBehaviour
+{
+	public GameObject ToEnable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnEnable()
+	{
+		ToEnable.SetActive(true);
+	}
+}
