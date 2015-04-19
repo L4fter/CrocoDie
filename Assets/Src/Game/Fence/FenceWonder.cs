8fb5882cc41cabd50a9d908cdf55e1f5e43967c8
Artem <a.korotenko@globalplaygames.com>
2015-04-19 04:12:36 +0300

    fence fixes

--- Assets/Src/Game/Fence/FenceWonder.cs
+++ Assets/Src/Game/Fence/FenceWonder.cs
@@ -11,6 +11,8 @@
 
         public float ScaleSpeed;
 
+        public float ScaleBase = 0.4f;
+
         public float WonderSpeed;
 
         private Vector3 start;
@@ -26,7 +28,7 @@
         {
             var sin = (float)Math.Sin(Time.time * this.WonderSpeed);
             this.transform.position = new Vector3(this.start.x + sin * this.Magnitude, this.start.y, this.start.z);
-            var size = 0.4f + ((float)Math.Sin(Time.time * this.ScaleSpeed) + 1) / 8;
+            var size = ScaleBase + ((float)Math.Sin(Time.time * this.ScaleSpeed) + 1) / 8;
             this.transform.localScale = new Vector3(size, size, size);
         }
     }
